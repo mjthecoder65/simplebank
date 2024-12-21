0e86d38448766d1cb918ceec499af82b65d40dbc
Michael <mjthecoder53161@gmail.com>
2024-12-21 11:34:29 +0900

    add utility package

---
+++ util/random.go
@@ -0,0 +1,47 @@
+package util
+
+import (
+	"strings"
+	"time"
+
+	"math/rand"
+)
+
+const ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var random *rand.Rand
+
+func init() {
+	seed := int64(time.Now().UnixNano())
+	random = rand.New(rand.NewSource(seed))
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + random.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var builder strings.Builder
+	k := len(ALPHABET)
+
+	for i := 0; i < n; i++ {
+		c := ALPHABET[random.Intn(k)]
+		builder.WriteByte(c)
+	}
+
+	return builder.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"EUR", "USD", "KRW", "CAD"}
+	n := len(currencies)
+	return currencies[random.Intn(n)]
+}
