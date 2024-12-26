9c5edb06a90e34c381b9fb24adbb126216dbf262
Michael <mjthecoder53161@gmail.com>
2024-12-26 20:15:16 +0900

    updated main.go

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+
+	_ "github.com/lib/pq"
+	"github.com/mjthecoder65/simplebank/api"
+	db "github.com/mjthecoder65/simplebank/db/sqlc"
+	"github.com/mjthecoder65/simplebank/util"
+)
 
 func main() {
-	fmt.Println("Hello, World")
+	config, err := util.LoadConfig(".")
+
+	if err != nil {
+		log.Fatal("failed to load config", err)
+		return
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+
+	if err != nil {
+		log.Fatal("failed to connect to the database:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+
+	if err != nil {
+		log.Fatal("cannot start server")
+	}
 }
