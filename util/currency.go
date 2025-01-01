1b73f854f309ca0eb9bec44357f8edd5b6badaf9
Michael <mjthecoder53161@gmail.com>
2025-01-01 19:20:10 +0900

    updated currency.go

--- util/currency.go
+++ util/currency.go
@@ -4,11 +4,13 @@ const (
 	USD = "USD"
 	EUR = "EUR"
 	CAD = "CAD"
+	KRW = "KRW"
+	TZS = "TZS"
 )
 
 func IsSupportedCurrency(currency string) bool {
 	switch currency {
-	case USD, EUR, CAD:
+	case USD, EUR, CAD, KRW, TZS:
 		return true
 	}
 	return false
